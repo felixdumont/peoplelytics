b4a7b6f9fc33fb0601be97b4101876e768e055ea
felixdumont <53409417+felixdumont@users.noreply.github.com>
2019-11-18 17:45:53 -0500

    Adding private settings

--- model/twilio.py
+++ model/twilio.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 import private_settings
 
 # Your Account SID from twilio.com/console
-account_sid = ""
+account_sid = private_settings.twilio_sid
 # Your Auth Token from twilio.com/console
 auth_token  = private_settings.twilio_key
 
@@ -10,7 +10,7 @@ def send_sms(driver):
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        to="",
-        from_="",
+        to=private_settings.twilio_to_phone,
+        from_=private_settings.twilio_from_phone,
         body="Hi Driver number {}. You are entering a high risk zone. Please be careful and lower your speed.".format(
             driver))
